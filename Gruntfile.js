503afda9efde36497c756566ac51616b98ab8deb
Grégory NEUT <g.neut@cotep.fr>
2018-12-13 17:37:33 +0100

    Push the sources

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    babel: {
+      options: {
+        sourceMap: true,
+      },
+      files: {
+        expand: true,
+        src: ['src/**/*.es6', 'test/**/*.es6'],
+        ext: '.js',
+        dest: './lib/',
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.registerTask('default', ['newer:babel']);
+};
